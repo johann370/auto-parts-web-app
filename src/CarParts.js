3a788c99e0b67ea16607d4d92a460435822e0c52
Johann Mendoza <johannmendoza370@gmail.com>
2022-11-24 14:24:00 -0600

    added fetch to get car parts

--- src/CarParts.js
+++ src/CarParts.js
@@ -1,122 +1,133 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CarPart from './CarPart'
 
 const CarParts = ({ addToCart, removeFromCart }) => {
+    const [displayParts, setDisplayParts] = useState(false);
+    const [carParts, setCarParts] = useState([]);
 
-    const carParts = [
-        {
-            id: 1,
-            name: 'Car Part',
-            price: 10.00,
-            manufacturer: 'manu',
-        },
-        {
-            id: 2,
-            name: 'Random',
-            price: 100.50,
-            manufacturer: 'ran',
-        },
-        {
-            id: 3,
-            name: 'Abc',
-            price: 0.50,
-            manufacturer: 'xyz',
-        },
-        {
-            id: 4,
-            name: 'Car Part',
-            price: 10.00,
-            manufacturer: 'manu',
-        },
-        {
-            id: 5,
-            name: 'Random',
-            price: 100.50,
-            manufacturer: 'ran',
-        },
-        {
-            id: 6,
-            name: 'Abc',
-            price: 0.50,
-            manufacturer: 'xyz',
-        },
-        {
-            id: 7,
-            name: 'Car Part',
-            price: 10.00,
-            manufacturer: 'manu',
-        },
-        {
-            id: 8,
-            name: 'Random',
-            price: 100.50,
-            manufacturer: 'ran',
-        },
-        {
-            id: 9,
-            name: 'Abc',
-            price: 0.50,
-            manufacturer: 'xyz',
-        },
-        {
-            id: 11,
-            name: 'Car Part',
-            price: 10.00,
-            manufacturer: 'manu',
-        },
-        {
-            id: 12,
-            name: 'Random',
-            price: 100.50,
-            manufacturer: 'ran',
-        },
-        {
-            id: 13,
-            name: 'Abc',
-            price: 0.50,
-            manufacturer: 'xyz',
-        },
-        {
-            id: 14,
-            name: 'Car Part',
-            price: 10.00,
-            manufacturer: 'manu',
-        },
-        {
-            id: 15,
-            name: 'Random',
-            price: 100.50,
-            manufacturer: 'ran',
-        },
-        {
-            id: 16,
-            name: 'Abc',
-            price: 0.50,
-            manufacturer: 'xyz',
-        },
-        {
-            id: 17,
-            name: 'Car Part',
-            price: 10.00,
-            manufacturer: 'manu',
-        },
-        {
-            id: 18,
-            name: 'Random',
-            price: 100.50,
-            manufacturer: 'ran',
-        },
-        {
-            id: 19,
-            name: 'Abc',
-            price: 0.50,
-            manufacturer: 'xyz',
-        },
-    ]
+    // const carParts = [
+    //     {
+    //         id: 1,
+    //         name: 'Car Part',
+    //         price: 10.00,
+    //         manufacturer: 'manu',
+    //     },
+    //     {
+    //         id: 2,
+    //         name: 'Random',
+    //         price: 100.50,
+    //         manufacturer: 'ran',
+    //     },
+    //     {
+    //         id: 3,
+    //         name: 'Abc',
+    //         price: 0.50,
+    //         manufacturer: 'xyz',
+    //     },
+    //     {
+    //         id: 4,
+    //         name: 'Car Part',
+    //         price: 10.00,
+    //         manufacturer: 'manu',
+    //     },
+    //     {
+    //         id: 5,
+    //         name: 'Random',
+    //         price: 100.50,
+    //         manufacturer: 'ran',
+    //     },
+    //     {
+    //         id: 6,
+    //         name: 'Abc',
+    //         price: 0.50,
+    //         manufacturer: 'xyz',
+    //     },
+    //     {
+    //         id: 7,
+    //         name: 'Car Part',
+    //         price: 10.00,
+    //         manufacturer: 'manu',
+    //     },
+    //     {
+    //         id: 8,
+    //         name: 'Random',
+    //         price: 100.50,
+    //         manufacturer: 'ran',
+    //     },
+    //     {
+    //         id: 9,
+    //         name: 'Abc',
+    //         price: 0.50,
+    //         manufacturer: 'xyz',
+    //     },
+    //     {
+    //         id: 11,
+    //         name: 'Car Part',
+    //         price: 10.00,
+    //         manufacturer: 'manu',
+    //     },
+    //     {
+    //         id: 12,
+    //         name: 'Random',
+    //         price: 100.50,
+    //         manufacturer: 'ran',
+    //     },
+    //     {
+    //         id: 13,
+    //         name: 'Abc',
+    //         price: 0.50,
+    //         manufacturer: 'xyz',
+    //     },
+    //     {
+    //         id: 14,
+    //         name: 'Car Part',
+    //         price: 10.00,
+    //         manufacturer: 'manu',
+    //     },
+    //     {
+    //         id: 15,
+    //         name: 'Random',
+    //         price: 100.50,
+    //         manufacturer: 'ran',
+    //     },
+    //     {
+    //         id: 16,
+    //         name: 'Abc',
+    //         price: 0.50,
+    //         manufacturer: 'xyz',
+    //     },
+    //     {
+    //         id: 17,
+    //         name: 'Car Part',
+    //         price: 10.00,
+    //         manufacturer: 'manu',
+    //     },
+    //     {
+    //         id: 18,
+    //         name: 'Random',
+    //         price: 100.50,
+    //         manufacturer: 'ran',
+    //     },
+    //     {
+    //         id: 19,
+    //         name: 'Abc',
+    //         price: 0.50,
+    //         manufacturer: 'xyz',
+    //     },
+    // ]
+
+    useEffect(() => {
+        fetch('http://localhost:5000/parts')
+            .then(response => response.json())
+            .then(data => {
+                setCarParts(data);
+                setDisplayParts(true);
+            });
+    });
 
     return (
         <div className='car-parts'>
-            {carParts.map((carPart) => <CarPart key={carPart.id} id={carPart.id} partName={carPart.name} price={carPart.price} manufacturer={carPart.manufacturer} addToCart={addToCart} removeFromCart={removeFromCart} />)}
+            {displayParts && carParts.map((carPart) => <CarPart key={carPart.id} id={carPart.id} partName={carPart.name} price={carPart.price} manufacturer={carPart.manufacturer} addToCart={addToCart} removeFromCart={removeFromCart} />)}
         </div>
     )
 }
